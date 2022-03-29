38025ca2b94431320bef637d728eabeffb1e66c6
choewy <choewy32@gmail.com>
2022-03-29 14:04:36 +0900

    환경변수의 포트 번호로 재설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 
 app.use(express.static('build'))
-app.get('/', (req,res)=>{
+app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/build/index.html');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('server ruuning');
 });
