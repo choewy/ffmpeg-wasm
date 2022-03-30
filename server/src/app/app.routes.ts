fed8b70c0bb53e2dfa301d930d59c5f538f36d4d
choewy <choewy32@gmail.com>
2022-03-30 13:42:44 +0900

    not found 처리(테스트)

--- server/src/app/app.routes.ts
+++ server/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { App } from "./app.interface"
 export const AppRoutes = (app: App, mode: string|undefined) => {
     app.use('/api/auth', UserRouter);
     if (mode === "production") {
+        console.log(1);
         app.get('*', (req: Request, res: Response)=>{
             res.sendFile(__dirname + '/build/index.html');
         });
