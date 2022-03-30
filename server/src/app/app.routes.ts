24e4009939871058ae6afeaafe0f5360842f29b9
choewy <choewy32@gmail.com>
2022-03-30 13:54:07 +0900

    not found 처리(테스트)

--- server/src/app/app.routes.ts
+++ server/src/app/app.routes.ts
@@ -1,19 +1,16 @@
-import { Request, Response } from "express";
+import { Request, Response, Router } from "express";
 import UserRouter from "../user/user.router";
 import { App } from "./app.interface"
 
+const AppRouter = Router();
+AppRouter.get('*', (req: Request, res: Response)=>{
+    console.log(`=========================`);
+    console.log(`====       GET *     ====`);
+    console.log(`=========================`);
+    res.sendFile(__dirname + '/build/index.html');
+});
+
 export const AppRoutes = (app: App, mode: string|undefined) => {
     app.use('/api/auth', UserRouter);
-
-    if (mode === "production") {
-        console.log(`=========================`);
-        console.log(`==== PRODUCTION MODE ====`);
-        console.log(`=========================`);
-        app.get('*', (req: Request, res: Response)=>{
-            console.log(`=========================`);
-            console.log(`====       GET *     ====`);
-            console.log(`=========================`);
-            res.sendFile(__dirname + '/build/index.html');
-        });
-    };
+    app.use('*', AppRouter);
 };
