c0d26a01eff7012c92db0f2b3ee973a518566049
choewy <choewy32@gmail.com>
2022-04-01 21:59:01 +0900

    Repository Reset

--- server/src/admin/admin.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { AuthCheck } from "../user/user.auth";
-import { AdminController } from "./admin.controller";
-
-const AdminRouter = Router();
-
-AdminRouter.get('/users', AuthCheck, AdminController.users);
-
-export default AdminRouter;
--- server/src/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserSign } from "../user/user.entities";
-declare global {
-  namespace Express {
-    interface Request {
-      user?: UserSign;
-      token?: string;
-    }
-  }
-}
