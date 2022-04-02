9b92aa083a97d8d1768d9e3b5ca8395018ae6f81
choewy <choewy32@gmail.com>
2022-04-02 21:36:52 +0900

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ import './03_interface_of_function';
 import './04_interface_of_class';
 import './05_literal_union_intersection_types';
 import './06_class-access-modifier';
+import './07_generics';
