774d888e0299a5701a7f588665b21435341621d1
choewy <choewy32@gmail.com>
2022-04-02 14:49:28 +0900

    함수 인터페이스 및 타입

--- src/03_interface_of_function.ts
+++ src/03_interface_of_function.ts
@@ -1,9 +1,66 @@
-interface NumberFunction {
+// interface
+interface TestFunction1 {
     (arg: number): number,
 };
 
-const numberFunction: NumberFunction = (arg) => {
+// use interface
+const testFunction1: TestFunction1 = (arg) => {
     return arg + 1;
 };
 
-const num = numberFunction(1);
+testFunction1(1);
+
+// use type
+const testFunction2 = (num: number, add?:number ):number => {
+    if (add) {
+        return num + add;
+    };
+    return num;
+};
+
+testFunction2(1, 2);
+
+// user type to array
+const testFunction3 = (...nums:number[]): number => {
+    let total = 0;
+    nums.forEach(num => {
+        total += num
+    });
+    return total;
+};
+
+testFunction3(1, 2, 3, 4, 5);
+
+// use bind
+interface What {
+    is: string;
+};
+
+function whatIsIt(this:What) {
+    console.log(this.is);
+};
+
+const what: What = {is: 'TypeScript'};
+const thisIsTypeScript = whatIsIt.bind(what);
+
+// use overload
+interface SomeThing {
+    name: string;
+    count: number;
+};
+
+function join(name: string, count: string): string;
+function join(name:string, count:number): SomeThing;
+function join (name: string, count: number | string): SomeThing | string {
+    if (typeof count === 'number') {
+        return {
+            name,
+            count
+        };
+    } else {
+        return 'count must number type';
+    };
+};
+
+const apple:string = join("apple", '2');
+const macBook:SomeThing = join("macBook", 2);
