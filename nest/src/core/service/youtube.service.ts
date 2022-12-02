2765752088bfbda792aed33338d95d06c5cccbc4
choewy <choewy32@gmail.com>
2022-12-02 19:18:13 +0900

    headless: false

--- nest/src/core/service/youtube.service.ts
+++ nest/src/core/service/youtube.service.ts
@@ -16,33 +16,37 @@ export class YoutubeService {
   }
 
   async upload(video: Video): Promise<void> {
-    upload(this.credentials, [
-      {
-        ...video,
-        channelName: this.credentials.channelName,
-        skipProcessingWait: true,
-        onSuccess: (url) => {
-          this.logger.verbose(`url : ${url}`);
-        },
-        onProgress: ({ stage, progress }) => {
-          switch (stage) {
-            case 0:
-              return this.logger.verbose(
-                `[uploading] ${video.title} : ${progress}%`,
-              );
+    upload(
+      this.credentials,
+      [
+        {
+          ...video,
+          channelName: this.credentials.channelName,
+          skipProcessingWait: true,
+          onSuccess: (url) => {
+            this.logger.verbose(`url : ${url}`);
+          },
+          onProgress: ({ stage, progress }) => {
+            switch (stage) {
+              case 0:
+                return this.logger.verbose(
+                  `[uploading] ${video.title} : ${progress}%`,
+                );
 
-            case 1:
-              return this.logger.verbose(
-                `[processing] ${video.title} : ${progress}%`,
-              );
+              case 1:
+                return this.logger.verbose(
+                  `[processing] ${video.title} : ${progress}%`,
+                );
 
-            case 2:
-              return this.logger.verbose(
-                `[done] ${video.title} : ${progress}%`,
-              );
-          }
+              case 2:
+                return this.logger.verbose(
+                  `[done] ${video.title} : ${progress}%`,
+                );
+            }
+          },
         },
-      },
-    ]);
+      ],
+      { headless: false },
+    );
   }
 }
